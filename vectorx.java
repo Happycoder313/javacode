103f7bfaef7bd58b57e7c2663ce380699f315523
Happycoder313 <nakulsuryawanshi00@gmail.com>
2025-01-23 10:57:38 +0530

    Add files via upload

---
+++ vectorx.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class vectorx {
+    public static void main(String[] args) {
+        Vector<String> v = new Vector<String>();
+        v.add("Apple");
+        v.add("Banana");
+        v.add("Mango");
+        v.add("Cherry");
+        System.out.println("Fruits are:" + v);
+        
+    }
+}
