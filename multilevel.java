1bcfc72c6a29432da7bfbc40ad84238c7e951d7b
Happycoder313 <nakulsuryawanshi00@gmail.com>
2025-01-28 12:09:34 +0530

    Add files via upload

---
+++ multilevel.java
@@ -0,0 +1,29 @@
+
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
+
+class Puppy extends Dog {
+    void play() {
+        System.out.println("Puppy plays");
+    }
+}
+
+ class multilevel {
+    public static void main(String args[]) {
+        Puppy puppy = new Puppy();
+        puppy.sound(); 
+        puppy.bark();  
+        puppy.play();  
+    }
+}
